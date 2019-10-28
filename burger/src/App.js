7ca610c5b91e37e60c78297dd7c2b4a7fcc38cc1
hhalici <hhalici@LTPRAK1>
2019-10-28 13:48:51 +0100

    init

---
+++ burger/src/App.js
@@ -0,0 +1,18 @@
+import React, {Components} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Components{
+  render(){
+
+  
+ return(
+ <div>
+
+  </div>
+ );
+  }
+}
+
+export default App;
